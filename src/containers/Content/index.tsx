51448b69a7b35790a6d886acccc95f4bcb69d3a0
Jhon Erik <javilaortiz@gmail.com>
2023-08-21 07:49:54 -0500

    Feature/logo (#13)
    * feat: passing data to brand component
    
    * feat: adding new libraries for commit
    
    BREAKING CHANGE: adding commitizen and husky
    
    * fix(component): branding logo according location done

--- src/containers/Content/index.tsx
+++ src/containers/Content/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../../styles/Content.scss';
 import Brand from '../../components/Brand';
+import { WeatherContext } from '../../services/Context/WeatherContext';
 
 const Content: React.FC = () => {
+  const weatherContext = useContext(WeatherContext);
+  const location = weatherContext?.location || 'London';
+
   return (
-  <div className="content">
-    <div className="content__inner-component">
-      <Brand />
+    <div className="content">
+      <div className="content__inner-component">
+        <Brand country={location} />
+      </div>
     </div>
-  </div>
   );
 };
 
